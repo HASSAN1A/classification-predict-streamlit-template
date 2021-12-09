ae6e7e2929bec7f3e88ac764d44daf6c4f111232
HASSAN1A <okothhassanjuma@gmail.com>
2021-12-10 00:44:14 +0300

    add multinomial

--- base_app.py
+++ base_app.py
@@ -64,7 +64,7 @@ def main():
 
 	# Building out the predication page
 	if selection == "Prediction":
-		st.info("Prediction with ML Logistic Regression with TF-IDF Vectorizer, multiclass classification model and GridSearchCV")
+		st.info("Prediction with Multinomial Logistic Regression with TF-IDF Vectorizer and GridSearchCV")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
 
