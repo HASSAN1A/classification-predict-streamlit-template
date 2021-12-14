45571a0110f47c6ccb9435881cc970e53a69fe03
HASSAN1A <okothhassanjuma@gmail.com>
2021-12-14 23:31:08 +0300

    add EDA

--- base_app.py
+++ base_app.py
@@ -23,17 +23,127 @@
 """
 # Streamlit dependencies
 import streamlit as st
-import joblib,os
+import joblib, os
 from PIL import Image
+
 # Data dependencies
 import pandas as pd
+import numpy as np
+
+# Data plots
+from textblob import TextBlob
+
+# from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import altair as alt
+
+# data manipulation
+import string
+import regex as re
+
+# word tokenize
+from nltk.tokenize import word_tokenize, TreebankWordTokenizer
+import nltk
+from nltk.corpus import stopwords
+
 
 # Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
-tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
+news_vectorizer = open("resources/vectorizer_3.pkl", "rb")
+tweet_cv = joblib.load(news_vectorizer)  # loading your vectorizer from the pkl file
 
-# Load your raw data
+# hash tags negative class
 raw = pd.read_csv("resources/train.csv")
+
+# Load Anti class data
+anti_hash = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_anti_hashtags.csv"
+)
+anti_retweet = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_Anti_retweets.csv"
+)
+
+# Load Anti class data
+neutral_hash = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_Neutral_hashtags.csv"
+)
+neutral_retweet = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_Neutral_retweet.csv"
+)
+
+# Load Anti class data
+pro_hash = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_Pro_hashtags.csv"
+)
+pro_retweet = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_Pro_retweets.csv"
+)
+
+# Load Anti class data
+news_hash = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_News_hashtags.csv"
+)
+news_retweet = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_News_retweets.csv"
+)
+
+# Load word count dataframe
+token_df = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/df_token_ind.csv"
+)
+
+# getting the analysis data frame
+clean_df = pd.read_csv(
+    "https://github.com/kabirodavies/streamlit/blob/master/resources/data/clean_df.csv"
+)
+
+
+def plot_target_based_features(feature):
+    
+    fig, ax = plt.subplots(1, 1, "all", figsize=(15, 9))
+    # create data frames for classes
+    x1 = clean_df[clean_df["sentiment"] == 1][feature]
+    x2 = clean_df[clean_df["sentiment"] == 2][feature]
+    x0 = clean_df[clean_df["sentiment"] == 0][feature]
+    x_neg = clean_df[clean_df["sentiment"] == -1][feature]
+    # plt.figure(1, figsize = (16, 8))
+    plt.xlabel("number of charecters in a tweet")
+    plt.ylabel("number of tweets")
+    plt.title("Word distribution chart")
+
+    _ = plt.hist(x1, alpha=0.5, color="grey", bins=50, label="belivers")
+    _ = plt.hist(x2, alpha=0.5, color="blue", bins=50, label="news")
+    _ = plt.hist(x0, alpha=0.6, color="green", bins=50, label="neutral")
+    _ = plt.hist(x_neg, alpha=0.5, color="orange", bins=50, label="anti")
+    plt.legend(["belivers", "news", "neutral", "anti"])
+
+    st.pyplot(fig)
+
+    return _
+
+
+# Making sentences for word  cloud retweets
+tweet_pro = " ".join(
+    [review for review in pro_retweet.Retweets if review is not np.nan]
+)
+tweet_neutral = " ".join(
+    review for review in neutral_retweet.Retweets if review is not np.nan
+)
+tweet_news = " ".join(
+    review for review in news_retweet.Retweets if review is not np.nan
+)
+tweet_anti = " ".join(
+    review for review in anti_retweet.Retweets if review is not np.nan
+)
+
+# Making sentences for word cloud hashtags
+hash_pro = " ".join([review for review in pro_hash.hashtags if review is not np.nan])
+hash_neutral = " ".join(
+    review for review in neutral_hash.hashtags if review is not np.nan
+)
+hash_news = " ".join(review for review in news_hash.hashtags if review is not np.nan)
+hash_anti = " ".join(review for review in anti_hash.hashtags if review is not np.nan)
+
+
 
 # The main function where we will build the actual app
 def main():
